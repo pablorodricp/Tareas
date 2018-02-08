887e1fc4d23edc318e9bca6decc5446f9e3867a7
Pablo <pablorodricp@gmail.com>
2018-02-08 21:19:35 +0100

    08/02/2018

---
+++ Pokemoncasigo/src/Pokemon.java
@@ -0,0 +1,19 @@
+
+public class Pokemon {
+	
+	private String nombre;
+	
+	private double fuerza;
+	
+	public Pokemon(int huevo) {
+		
+		switch (huevo) {
+		case 0:
+			break;
+		case 1:
+			break;
+		case 2:
+			break;
+		}
+	}
+}
